87e77fc5ccc4944bdffeb49c272408190595d21c
tHomas H <tom@mathpunk.net>
2017-06-13 20:16:50 -0700

    Checkpoint

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/algo.generic "0.1.2"]]
+                 [org.clojure/algo.generic "0.1.2"]
+                 [org.clojure/math.combinatorics "0.1.4"]]
   :main ^:skip-aot fun-with-primes.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/fun_with_primes/core.clj
+++ src/fun_with_primes/core.clj
@@ -1,5 +1,8 @@
 (ns fun-with-primes.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.math.combinatorics :refer [cartesian-product]]))
+
+;; I've implemented a Sieve of Eratosthenes, since a bit of research seemed to suggest that its efficiency-to-complexity ratio is not terrible. The Sieve of Atkin reportedly has better asymptotic complexity, but requires a bit of quadratic solving that it's not immediately obvious how to do efficiently.
 
 (defn is-multiple? [modulus number]
   (when (or (not (integer? modulus))
@@ -12,6 +15,17 @@
   (remove (partial is-multiple? modulus) numbers))
 
 (defn first-primes [n]
+  (loop [primes []
+         candidates (range 2 n)]
+    (if (empty? candidates)
+      primes
+      (let [next-prime (first candidates)
+            next-candidates (sieve next-prime candidates)]
+        (recur (conj primes next-prime)
+               next-candidates)))))
+
+;; This is actually my first prime finder for this problem, but it didn't seem like the transients were giving much of a boost. I must not understand when they are worthwhile.
+(defn first-primes-with-transients [n]
   (loop [primes (transient [])
          candidates (range 2 n)]
     (if (empty? candidates)
@@ -21,7 +35,46 @@
         (recur (conj! primes next-prime)
                next-candidates)))))
 
+;; The data for our multiplication table.
+(defn prime-products
+  "The rows of the table, with a 1 in the corner."
+  [n]
+  (let [numbers (into [1] (first-primes n))
+        table-width (count numbers)
+        pairs (cartesian-product numbers numbers)
+        products (map #(apply * %) pairs)]
+    (partition table-width products)))
+
+;; The string representation for our table.
+
+(defn pad
+  "Given a length and an integer, return the string with spaces prepending that integer's string representation."
+  [padding n]
+  (let [number (str n)]
+    (str (apply str (repeat (- padding (count number)) " ")) number)))
+
+
+(->> (flatten (prime-products 10))
+     (map str)
+     (map count)
+     (reduce max))
+
+(defn print-table [n]
+  (let [data (prime-products n)
+        padding (->> (flatten data)
+                     (map str)
+                     (map count)
+                     (reduce max))
+        pretty-data (map (fn [row]
+                           (map (fn [entry] (pad padding entry))
+                                row) data))]
+    (map println pretty-data)))
+
+(print-table 10)
+
+
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println (first-primes args)))
+  [arg]
+  (println (first-primes (Integer. arg))))
